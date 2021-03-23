f38d5a83ee4b63c143dafc6acdd2077ec012d3b8
Focusucof <imthefirstfocus@gmail.com>
2021-03-23 09:28:08 -0400

    fixed getItemId.ts; now writes to a json file

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,10 @@ dotenv.config();
 let playerID : string = '10a19205-2c9b-5103-a689-ed80299bc19a';
 let entitlementToken: string = `${process.env.entitlementToken}`;
 let authToken : string = `${process.env.authToken}`;
+let clientVersion : string = 'release-02.05-shipping-4-533692';
 
 getRankInfo.getRankInfo(playerID, entitlementToken, authToken);
 getUserFromID.getUserFromID(playerID, entitlementToken, authToken);
-//getPlayerStore.getPlayerStore(playerID, entitlementToken, authToken);
-getItemID.getItemID(playerID, entitlementToken, authToken);
+getPlayerStore.getPlayerStore(playerID, entitlementToken, authToken);
+getItemID.getItemID(playerID, entitlementToken, authToken, clientVersion);
 
--- src/requests/getItemID.ts
+++ src/requests/getItemID.ts
@@ -1,24 +1,34 @@
 import * as request from 'request';
+import * as fs from 'fs';
 
 module.exports = {
-    getItemID: function(playerID, entitlementToken, authToken) {
+    getItemID: function(playerID, entitlementToken, authToken, clientVersion) {
+
         var options = {
             'method': 'GET',
             'url': 'https://shared.na.a.pvp.net/content-service/v2/content',
             'headers': {
                 'X-Riot-Entitlements-JWT': entitlementToken,
-                //'X-Riot-ClientPlatform': 'ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9',
+                'X-Riot-ClientVersion': clientVersion,
+                'X-Riot-ClientPlatform': 'ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9',
                 'Authorization': 'Bearer ' + authToken,
-                'X-Riot-ClientVersion': "release-02.05-shipping-4-533692"
+                //'Cookie': '__cfduid=d689ccac7b1308094953f3da9b7ff9fc41615996959'
             }
-        }
+        };
 
-        request(options, function(error, response) {
-            if(error) {
+        request(options, function (error, response) {
+            if (error) {
                 console.log(error);
             }
-            console.log(JSON.parse(response.body));
+            //console.log(JSON.parse(response.body));
+            fs.writeFile('items.json', response.body, function(err) {
+                if(err) {
+                  console.log(err);
+                } else {
+                  console.log("JSON saved to items.json");
+                }
+            }); 
         });
-    
+        
     }
 }
